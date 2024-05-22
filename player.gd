66dc917c1ff00a5d7408ab35630d953dbbfb4bfd
platz-halter <simon.pressel2@proton.me>
2024-05-22 18:42:41 +0200

    Fixed small bug that breaks speed

--- player.gd
+++ player.gd
@@ -1,6 +1,6 @@
 extends CharacterBody2D
 
-@export var max_speed 		= 10
+@export var max_speed 		= 50
 @export var jump_power 		= 50
 @export var local_grav 		= 2
 @export var jump_cooldown 	= 2
@@ -16,17 +16,16 @@ func _physics_process(delta):
 	velocity.y += local_grav
 
 	if Input.is_action_pressed("ui_right"):
-		velocity.x += max_speed
+		velocity.x = max_speed
 		$player_sprite.flip_h = true
 	elif Input.is_action_pressed("ui_left"):
-		velocity.x -= max_speed
+		velocity.x = -max_speed
 		$player_sprite.flip_h = false
 	else:
 		velocity.x = 0
 	if is_on_floor():
 		if Input.is_action_just_pressed("ui_up"):
 			velocity.y = -jump_power
-	dash_speed = 0
 	move_and_slide()
  
 
