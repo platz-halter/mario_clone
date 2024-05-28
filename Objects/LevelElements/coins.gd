ab7861b502f94288e28f044092870010e6586491
platz-halter <simon.pressel2@proton.me>
2024-05-28 21:01:47 +0200

    Added coins

---
+++ Objects/LevelElements/coins.gd
@@ -0,0 +1,13 @@
+extends Area2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	if not is_in_group("coins"):
+		print("DEL ME NOW")
+		self.queue_free()
