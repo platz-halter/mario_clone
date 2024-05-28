322917db8b4097f1ca1364542e1eebf064d41db2
platz-halter <simon.pressel2@proton.me>
2024-05-28 15:50:32 +0200

    cleanup

--- world.gd
+++ world.gd
@@ -1,29 +1,35 @@
 extends Node2D
 
+#Preload all needed levels 
 var level1 = preload("res://Levels/level_1.tscn")
 var level2 = preload("res://Levels/level_2.tscn")
 
-var player_bank
+var player_bank #Cash that the player collects before stashing it in the "lobby"
 
-func load_level(level):
-	var inst_level = level.instantiate()
-	add_child(inst_level)
-func get_level():
+func get_level(): #Get currently loaded level
 	var level_node = get_child(0)
 	return level_node
 
-func _ready():
-	player_bank = 0
+func load_level(level): #Load level and position player
+	remove_child(get_level())
+	var inst_level = level.instantiate()
+	add_child(inst_level)
 	
 	move_child($Player, 1) #Make Player stay on top layer
 	var t = get_child(0)
 	t = t.get_child(0)
 	$Player.position = t.position
 	
+func _ready(): #Executes on game start
+	player_bank = 0
+	
+	load_level(level1)
 	print(get_tree_string_pretty())
+
 func _process(delta): #System UI
 	if Input.is_action_just_pressed("sys_exit"):
-		get_tree().quit()
+		load_level(level2)
+		#get_tree().quit()
 	#print($player.position) --> DEBUG
 
 	if $Player.position.y >= 90:
